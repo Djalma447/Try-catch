4271793b5b5eea4dba30df12e3de117af2d3cad7
Djalma <djalma.ant@gmail.com>
2020-02-05 01:43:59 -0300

    Exceptions Created

--- Course/Program.cs
+++ Course/Program.cs
@@ -6,7 +6,23 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+                int result = n1 / n2;
+
+                Console.WriteLine(result);
+            }
+
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Division by zero is IMPOSSIBLE");
+            }
+            catch (FormatException a)
+            {
+                Console.WriteLine("Format Error! " + a.Message);
+            }
         }
     }
 }
